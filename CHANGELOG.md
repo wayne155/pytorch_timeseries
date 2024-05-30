14825597ffc3d40b6cc8b13f48039e5e2932c34b
WanyeYip <wwye155@gmail.com>
2024-05-30 05:47:20 +0000

    ready for v0.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## 0.0.3
+fixed: dependencies issue of torch-timeserie pypi package 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = '0.0.3a3'
+__version__ = '0.0.3'
 URL = 'https://github.com/wayne155/pytorch_timeseries'
 
 install_requires = [
